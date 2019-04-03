a0d187ff50c30f4a52575800963d120aad3d8d2b
CarlosCavada <47177678+CarlosCavada@users.noreply.github.com>
2019-04-03 18:25:09 +0800

    Ejercicio guiado y micropractica fire base
    solucion

---
+++ microp_4/Agenda/src/app/firebase.credentials.ts
@@ -0,0 +1,14 @@
+
+<script src="https://www.gstatic.com/firebasejs/5.9.2/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCUYr_TpoeYimZd0pZYGNmUuUsGs3CRUy8",
+    authDomain: "nombre-957c5.firebaseapp.com",
+    databaseURL: "https://nombre-957c5.firebaseio.com",
+    projectId: "nombre-957c5",
+    storageBucket: "nombre-957c5.appspot.com",
+    messagingSenderId: "561699821713"
+  };
+  firebase.initializeApp(config);
+</script>
